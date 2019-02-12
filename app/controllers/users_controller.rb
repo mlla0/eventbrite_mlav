e0bc72e1b3cbcc3f95d36a563e3b53c5d5389206
Antoine Vercoutère <antoine@mlav.land>
2019-02-12 21:36:20 +0100

    add of user authentification for event creation

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!, only: [:show]
-  before_action :auth_user, only: [:show]
+  before_action :authenticate_user!, only: [:show, :edit, :update]
+  before_action :auth_user, only: [:show, :edit]
 
   def show
   	@user = User.find(params[:id])
