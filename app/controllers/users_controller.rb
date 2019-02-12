fe4ddaa6a3a9099ccc050473391b4c3e283c7626
mlla <maud@mlav.land>
2019-02-12 19:27:55 +0100

    add of edit profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,20 @@ class UsersController < ApplicationController
   	@events = Event.where(admin: @user).order('start_date ASC')
   end
 
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+
+    if @user.update(first_name: params[:first_name], last_name: params[:last_name], description: params[:description])
+      redirect_to user_path(current_user.id), success: "Your profile has been successfully edited !"
+    else
+      redirect_to edit_gossip_path(params[:id]), danger: "#{@gossip.errors.full_messages.join(". ")}"
+    end  
+  end
+
   private
 
   def auth_user
