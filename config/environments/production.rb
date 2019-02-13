56e07e11a8979c8f8e46a70985903899ba3306f9
Antoine Vercoutère <antoine@mlav.land>
2019-02-13 10:29:01 +0100

    add of mailer preferences

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
 
-  config.action_mailer.default_url_options = { :host => 'YOURAPPNAME.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'poow-event.herokuapp.com' }
   
   # Settings specified here will take precedence over those in config/application.rb.
 
